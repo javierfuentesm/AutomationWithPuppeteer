1d155942e06c21e06a07d989241588a30eb836f8
Javier <javier.fuentesm@hotmail.com>
2020-06-08 23:05:27 -0400

    Wait for

--- tests/example.test.js
+++ tests/example.test.js
@@ -4,11 +4,12 @@ describe('My first puppeter test', () => {
 	it('Should  launch the browser', async () => {
 		const browser = await puppeteer.launch({
 			headless: false,
-			slowMo: 50,
+			slowMo: 10,
 			devtools: false,
 		})
 		const page = await browser.newPage()
 		await page.goto('https://google.com')
+		await page.waitFor(3000)
 		await page.waitForSelector('.gb_Fa')
 		await browser.close()
 	})
