296de916a7ee3b718ddf25510ffce27dacb5718c
Javier <javier.fuentesm@hotmail.com>
2020-06-08 23:59:49 -0400

    Selection an option in select filling an input text and clicking in the submit button

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,13 +1,13 @@
 const puppeteer = require('puppeteer')
 
 describe('My first puppeter test', () => {
-	it('Should  launch the browser', async () => {
-			const browser = await puppeteer.launch({
-				headless: false,
-				slowMo: 10,
-				devtools: false,
-			})
-			const page = await browser.newPage()
+	it('Should  fill th input and click the checkbox ', async () => {
+		const browser = await puppeteer.launch({
+			headless: false,
+			slowMo: 10,
+			devtools: false,
+		})
+		const page = await browser.newPage()
 		try {
 			await page.goto('https://devexpress.github.io/testcafe/example/')
 			await page.type('#developer-name', 'Javier', { delay: 0 })
@@ -15,6 +15,28 @@ describe('My first puppeter test', () => {
 			await page.click('#tried-test-cafe', { clickCount: 1, delay: 0 })
 			await page.waitFor(2000)
 			await page.click('#tried-test-cafe', { clickCount: 1, delay: 0 })
+			await page.waitFor(5000)
+			await page.close()
+		} catch (error) {
+			console.error(error)
+			await page.close()
+		}
+	})
+	it('Should select an option of the dropdown and write the text in the textarea and  click on the submit', async () => {
+		const browser = await puppeteer.launch({
+			headless: false,
+			slowMo: 10,
+			devtools: false,
+		})
+		const page = await browser.newPage()
+		try {
+			await page.goto('https://devexpress.github.io/testcafe/example/')
+			await page.type('#developer-name', 'Javier', { delay: 0 })
+			await page.click('#tried-test-cafe', { clickCount: 1, delay: 0 })
+			await page.waitFor(2000)
+			await page.select('#preferred-interface', 'JavaScript API')
+			await page.type('#comments', 'Un texto de prueba ')
+			await page.click('#submit-button')
 			await page.waitFor(5000)
 			await page.close()
 		} catch (error) {
