8d3bdd9cd252a0d06f1c3ce8ed24401e2469b2b6
Javier <javier.fuentesm@hotmail.com>
2020-06-08 23:44:45 -0400

    clicking checkbox

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,15 +2,24 @@ const puppeteer = require('puppeteer')
 
 describe('My first puppeter test', () => {
 	it('Should  launch the browser', async () => {
-		const browser = await puppeteer.launch({
-			headless: false,
-			slowMo: 10,
-			devtools: false,
-		})
-		const page = await browser.newPage()
-		await page.goto('https://devexpress.github.io/testcafe/example/')
-		await page.type('#developer-name', 'Javier', { delay: 0 })
-		await page.waitFor(5000)
-		await page.close()
+			const browser = await puppeteer.launch({
+				headless: false,
+				slowMo: 10,
+				devtools: false,
+			})
+			const page = await browser.newPage()
+		try {
+			await page.goto('https://devexpress.github.io/testcafe/example/')
+			await page.type('#developer-name', 'Javier', { delay: 0 })
+			await page.waitFor(2000)
+			await page.click('#tried-test-cafe', { clickCount: 1, delay: 0 })
+			await page.waitFor(2000)
+			await page.click('#tried-test-cafe', { clickCount: 1, delay: 0 })
+			await page.waitFor(5000)
+			await page.close()
+		} catch (error) {
+			console.error(error)
+			await page.close()
+		}
 	})
 })
