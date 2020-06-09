cebe770a7b56a1b6a9f9b4d5c60f121cd7538cba
Javier <javier.fuentesm@hotmail.com>
2020-06-08 23:03:07 -0400

    Slow motion and headless execution

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,7 +2,11 @@ const puppeteer = require('puppeteer')
 
 describe('My first puppeter test', () => {
 	it('Should  launch the browser', async () => {
-		const browser = await puppeteer.launch({ headless: false })
+		const browser = await puppeteer.launch({
+			headless: false,
+			slowMo: 50,
+			devtools: false,
+		})
 		const page = await browser.newPage()
 		await page.goto('https://google.com')
 		await page.waitForSelector('.gb_Fa')
