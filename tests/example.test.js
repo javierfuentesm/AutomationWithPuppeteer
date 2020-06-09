dc3ab0bd858a9008bdc2006fc09b088f96751db8
Javier <javier.fuentesm@hotmail.com>
2020-06-09 13:30:04 -0400

    Wait for xpath

--- tests/example.test.js
+++ tests/example.test.js
@@ -93,14 +93,56 @@ describe('My first puppeter test', () => {
 		try {
 			await page.goto('https://google.com')
 			//Full XPath
-			await page.waitForXPath('/html/body/div/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input')
+			await page.waitForXPath(
+				'/html/body/div/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input'
+			)
 			//Xpath
-			await page.waitForXPath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+			await page.waitForXPath(
+				'//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input'
+			)
 			// Clase de un elemento mas arriba
 			await page.waitForSelector('.RNNXgb')
 
 			await page.type('.RNNXgb', 'Que onda hoomies')
 			await page.keyboard.press('Enter', { delay: 10 })
+			await page.waitFor(2000)
+		} catch (error) {
+			console.error(error)
+		} finally {
+			await page.close()
+		}
+	})
+	it('Should validate if the button  doesnt exist', async () => {
+		const browser = await puppeteer.launch({
+			headless: false,
+			slowMo: 10,
+			devtools: false,
+		})
+		const page = await browser.newPage()
+		let pages = await browser.pages()
+		await pages[0].close()
+		try {
+			await page.goto('https://google.com')
+			//Full XPath
+			await page.waitForXPath(
+				'/html/body/div/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input'
+			)
+
+			await page.type('.RNNXgb', 'Que onda hoomies')
+			await page.keyboard.press('Enter', { delay: 10 })
+			//Validate document doesnt exist you can get this selector from googlw chrome devtools
+			await page.waitFor(
+				() =>
+					!document.querySelector(
+						'#tsf > div:nth-child(2) > div.A8SBwf.emcav > div.RNNXgb > div > div.a4bIc > input'
+					)
+			)
+			//Validate that the element doesnt exist  with the waitforselector/waitforxpath
+			await page.waitForXPath(
+				'/html/body/div/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input',
+				{ hidden: true, timeout: 3000 }
+			)
+
 			await page.waitFor(2000)
 		} catch (error) {
 			console.error(error)
