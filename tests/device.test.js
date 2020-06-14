4386cf7f7082bb569d19644c287c3ea7eeea0dac
Javier <javier.fuentesm@hotmail.com>
2020-06-14 00:12:57 -0400

    Device emulation

--- tests/device.test.js
+++ tests/device.test.js
@@ -8,8 +8,29 @@ describe('Device Emulation', () => {
 			slowMo: 10,
 			devtools: false,
 		})
-		page = await puppeteer.newPage()
+		page = await browser.newPage()
 		await page.setDefaultTimeout(10000)
 		await page.setDefaultNavigationTimeout(20000)
+	})
+	after(async () => {
+		await browser.close()
+	})
+	it('Desktop device ', async () => {
+		await page.setViewport({ width: 1650, height: 1050 })
+		await page.goto('https://google.com')
+		await page.waitFor(5000)
+	})
+	it('Tablet device ', async () => {
+		const tablet = puppeteer.devices['iPad landscape']
+		await page.emulate(tablet)
+		await page.goto('https://google.com')
+
+		await page.waitFor(5000)
+	})
+	it('Phone device ', async () => {
+		const mobile = puppeteer.devices['iPhone X']
+		await page.emulate(mobile)
+		await page.goto('https://google.com')
+		await page.waitFor(5000)
 	})
 })
